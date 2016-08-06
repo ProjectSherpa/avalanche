0ea6ba7d131d9bc73da48fab87e52ad3266cad1c
wayneadams <wadams19@gmail.com>
2016-08-06 12:16:04 -0700

    Refactor to use loadtest to capture result logs

--- benchmark.js
+++ benchmark.js
@@ -1,9 +1,13 @@
-var seige = require('siege');
+const loadtest = require('loadtest');
 
-seige()
-  .host('10.8.24.125')
-  .on(5000)
-  .concurrent(3000)
-  .for(100000).times
-  .get('/')
-  .attack()
+var options = {
+  url: 'http://10.8.24.125:5000',
+  maxRequests: 100,
+}
+
+loadtest.loadTest(options, function(err, result) {
+  if (err) {
+    return console.error('Got an error', err);
+  }
+  console.log('Tests run successfully with results of:', result);
+});
