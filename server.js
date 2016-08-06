60b6b9cc9628a011f9fb078d2db483b19f04d6a1
Jeremy Toce <jeremytoce@gmail.com>
2016-08-06 16:06:16 -0700

    Updated front-end dash request

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const server = http.createServer(app);
 
 const loadtest = require('loadtest');
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-var data = {url: 'http://10.8.24.125:5000/', maxSeconds: 20, concurrency: 20}
-
 app.post('/avalanche', function(req, res) {
-  loadtest.loadTest(data, function(err, result) {
+  loadtest.loadTest(req.body, function(err, result) {
     if (err) {
       return console.error('Got an error', err);
     }
